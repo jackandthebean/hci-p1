76e5086918cdcbeb2db482d719e717ff519ef196
jackandthebean <jacquelinelee@uchicago.edu>
2022-10-18 22:43:07 -0500

    Redesign and almost done with csv stuff

--- sketch.js
+++ sketch.js
@@ -1,39 +1,53 @@
+/*
+weird mapping game for hci
+by team Hot Coding Individuals
+
+Keyboard Options:
+'s' to start
+
+*/
+
+// Data
+const rows = [];
+
+// Design Variables
 let screen_horizontal = window.innerWidth;
 let screen_vertical = window.innerHeight;
-let left = 20;
-
-let finger_board;
-let finger_board_width = screen_horizontal/4;
-let finger_board_height = finger_board_width * 650 / 850;
+let margin = 20;
 
 let game_board_width = 450;
-//let game_board_height = 450;
-//let target_zone_height = 450;
-let game_board_x = screen_horizontal - game_board_width - left;
-let game_board_y = 50;
-//let target_zone_y = 450;
+let game_board_height = 500;
+let game_board_x = screen_horizontal - game_board_width - margin;
+let game_board_y = screen_vertical - game_board_height - margin;
 
+let target_zone_height = 50;
+let target_zone_y = game_board_y + game_board_height - target_zone_height;
+
+let dot_diameter = 30;
+let line_interval = 75;
+
+// x values for lines on main board
+var one = game_board_x + line_interval;
+var two = game_board_x + line_interval * 2;
+var three = game_board_x + line_interval * 3;
+var four = game_board_x + line_interval * 4;
+var five = game_board_x + line_interval * 5;
+
+// Finger Board Image
+let finger_board;
+let finger_board_height = screen_vertical/4;
+let finger_board_width = finger_board_height * 850 / 650;
+
+// Game State
 var run_game = false;
 var dots = [];
 var dot_speed = 2;
-var normal_rounds = 3;
-var weird_rounds = 6;
+var normal_rounds = 1;//3;
+var weird_rounds = 1;//6;
 var normal_game = true;
-
-// x values for main board
-var one = game_board_x + 75;
-var two = game_board_x + 75*2;
-var three = game_board_x + 75*3;
-var four = game_board_x + 75*4;
-var five = game_board_x + 75*5;
-
 
 var assigned_nums = '1  2  3  4  5';
 var inputs = ['1', '2', '3', '4', '5']
-
-var num_rounds = 1;
-
-var total_hits = 0;
 
 var key1 = false;
 var key2 = false;
@@ -41,14 +55,45 @@ var key3 = false;
 var key4 = false;
 var key5 = false;
 
+var num_rounds = 1;
+
+var total_hits = 0;
+
+var one_hits = 0;
+var two_hits = 0;
+var three_hits = 0;
+var four_hits = 0;
+var five_hits = 0;
+
+var ones = 0;
+var twos = 0;
+var threes = 0;
+var fours = 0;
+var fives = 0;
+
 // y value of the last note created
-var last_y = 75;
+var last_y = game_board_y + dot_diameter/2;
 
 function preload(){
   finger_board = loadImage('finger_board.png');
 }
 
 function setup() {
+  let row = [];
+  row.push("Round Number");
+  row.push("Normal Round?");
+  row.push("One Hits");
+  row.push("Ones Total");
+  row.push("Two Hits");
+  row.push("Twos Total");
+  row.push("Three Hits");
+  row.push("Threes Total");
+  row.push("Four Hits");
+  row.push("Fours Total");
+  row.push("Five Hits");
+  row.push("Fives Total");
+  rows.push(row);
+
   createCanvas(screen_horizontal, screen_vertical);
   createDots();
 }
@@ -70,47 +115,47 @@ function createDots() {
 
 function draw_board() {
   // main board
-  fill(color(255, 204, 0));
-  rect(game_board_x, game_board_y, game_board_width, 500);
+  fill('#E4BE82');
+  rect(game_board_x, game_board_y, game_board_width, game_board_height);
 
   // note lines
-  line(one, 75, one, 475);
-  line(two, 75, two, 475);
-  line(three, 75, three, 475);
-  line(four, 75, four, 475);
-  line(five, 75, five, 475);
+  line(one, 0, one, game_board_y + game_board_height);
+  line(two, 0, two, game_board_y + game_board_height);
+  line(three, 0, three, game_board_y + game_board_height);
+  line(four, 0, four, game_board_y + game_board_height);
+  line(five, 0, five, game_board_y + game_board_height);
 
   // target zone
-  fill(color(0, 204, 0));
-  rect(game_board_x, 500, game_board_width, 50);
+  fill('#CBD125');
+  rect(game_board_x, target_zone_y, game_board_width, target_zone_height);
 }
 
 function draw_start_board() {
   // main board
-  fill(color(255, 204, 0));
-  rect(game_board_x, game_board_y, game_board_width, 500);
+  fill('#E4BE82');
+  rect(game_board_x, game_board_y, game_board_width, game_board_height);
 
   //instructions
-  fill(200, 0, 200);
+  fill('white');
   textSize(28);
   if (num_rounds <= normal_rounds + weird_rounds) {
-     text("Press 's' to start round " + num_rounds, 385, 300)
+     text("Press 's' to start round " + num_rounds, game_board_x + game_board_width/2 - 150, screen_vertical/2 - finger_board_height + 28)
     if (num_rounds == normal_rounds + 1){
-      text(" \t \t   ATTENTION: \n The keys have changed", 375, 380)
+      text(" \t \t   ATTENTION: \n The keys have changed", game_board_x + game_board_width/2 - 150, screen_vertical/2)
 
   }
 
 
   }
   else {
-    text("Thanks for playing :)", 400, 300)
+    text("Thanks for playing :)", game_board_x + game_board_width/2 - 150, screen_vertical/2)
   }
 
 
 
   // target zone
-  fill(color(0, 204, 0));
-  rect(game_board_x, 500, game_board_width, 50);
+  fill('#CBD125');
+  rect(game_board_x, target_zone_y, game_board_width, target_zone_height);
 }
 
 function manage_dots() {
@@ -118,31 +163,36 @@ function manage_dots() {
     dot['y'] += dot_speed;
 
     if (dot['hit'] == false) {
-      circle(dot['x'], dot['y'], 30);
+      circle(dot['x'], dot['y'], dot_diameter);
 
 
       // check if note is in target zone
-      if (dot['y'] >= 500 && dot['y'] <= 550 && dot['hit'] == false) {
+      if (dot['y'] >= target_zone_y && dot['y'] <= (target_zone_y + target_zone_height) && dot['hit'] == false) {
           // check if correct key for the column was pressed
           if(key1 && dot['x'] == one) {
             dot['hit'] = true;
             total_hits++;
+            one_hits++;
           }
           if(key2 && dot['x'] == two) {
             dot['hit'] = true;
             total_hits++;
+            two_hits++;
           }
           if(key3 && dot['x'] == three) {
             dot['hit'] = true;
             total_hits++;
+            three_hits++;
           }
           if(key4 && dot['x'] == four) {
             dot['hit'] = true;
             total_hits++;
+            four_hits++;
           }
           if(key5 && dot['x'] == five) {
             dot['hit'] = true;
             total_hits++;
+            five_hits++;
           }
         }
       }
@@ -154,15 +204,43 @@ function manage_dots() {
   key4 = false;
   key5 = false;
 
-  if (dots[29]['y'] > 600) {
+  if (dots[29]['y'] > screen_vertical) {
     end_round();
   }
 }
 
 //transition from one round to the next
 function end_round() {
+  let row = [];
+  row.push(num_rounds);
+  row.push(normal_game);
+  row.push(one_hits);
+  row.push(ones);
+  row.push(two_hits);
+  row.push(twos);
+  row.push(three_hits);
+  row.push(threes);
+  row.push(four_hits);
+  row.push(fours);
+  row.push(five_hits);
+  row.push(fives);
+  rows.push(row);
+
   dots = []
   total_hits = 0;
+
+  one_hits = 0;
+  two_hits = 0;
+  three_hits = 0;
+  four_hits = 0;
+  five_hits = 0;
+
+  ones = 0;
+  twos = 0;
+  threes = 0;
+  fours = 0;
+  fives = 0;
+
   num_rounds ++;
   if (num_rounds > normal_rounds){
     normal_game = false;
@@ -170,21 +248,21 @@ function end_round() {
     inputs = ['5', '1', '4', '3', '2']
   }
   run_game = false;
-  last_y = 75
+  last_y = game_board_y + dot_diameter/2;
   createDots();
 }
 
 function draw() {
   background('#92C471');
-  image(finger_board, left, screen_vertical/2 - finger_board_height, finger_board_width, finger_board_height);
-  
-  fill(200, 0, 200);
-  textSize(32);
-  text('Hits: ' + total_hits + '/30', 36, 440);
+  image(finger_board, margin, screen_vertical/2 - finger_board_height, finger_board_width, finger_board_height);
+
+  fill('white');
+  textSize(28);
+  text('Hits: ' + total_hits + '/30', margin, screen_vertical/2 + finger_board_height/2);
 
   if (run_game) {
     draw_board();
-    fill(color(255, 59, 0))
+    fill('#F0DEB3')
     manage_dots();
   }
 
@@ -196,7 +274,6 @@ function draw() {
 function keyPressed() {
   if (key == 's' && num_rounds <= normal_rounds + weird_rounds) {
     run_game = true;
-    document.getElementById("instruction").style = "display: none";
   }
 
   if (key == inputs[0]) {
@@ -215,5 +292,25 @@ function keyPressed() {
     key5 = true;
   }
 
+  if (key == 'd') {
+    download_results();
+}
+}
+
+function download_results() {
+  var csv = "data:text/csv;charset=utf-8,";
+
+  rows.forEach(function(rowArray) {
+    let row = rowArray.join(",");
+    csv += row + "\r\n";
+  });
+
+  var encodedUri = encodeURI(csv);
+  var link = document.createElement("a");
+  link.setAttribute("href", encodedUri);
+  link.setAttribute("download", "UkuHero.csv");
+  document.body.appendChild(link);
+
+  link.click();
 }
 
